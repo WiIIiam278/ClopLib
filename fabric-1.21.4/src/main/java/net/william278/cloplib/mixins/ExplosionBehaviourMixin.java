cae345aaf08e3b178868a2dc104beb322fecc8ff
William <will27528@gmail.com>
2025-01-01 12:06:20 +0000

    refactor: adjust explosion mixins

--- fabric-1.21.4/src/main/java/net/william278/cloplib/mixins/ExplosionBehaviourMixin.java
+++ fabric-1.21.4/src/main/java/net/william278/cloplib/mixins/ExplosionBehaviourMixin.java
@@ -19,25 +19,27 @@
 
 package net.william278.cloplib.mixins;
 
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.explosion.ExplosionImpl;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.explosion.AdvancedExplosionBehavior;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
 import net.william278.cloplib.events.ExplosionEvents;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
+@Mixin(value = {ExplosionBehavior.class, AdvancedExplosionBehavior.class})
+public class ExplosionBehaviourMixin {
 
-@Mixin(ExplosionImpl.class)
-public abstract class ExplosionImplMixin {
-
-    @Shadow
-    protected abstract List<BlockPos> getBlocksToDestroy();
-
-    @Redirect(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/explosion/ExplosionImpl;getBlocksToDestroy()Ljava/util/List;"))
-    private List<BlockPos> explodeMixin(ExplosionImpl instance) {
-        return ExplosionEvents.BEFORE_BLOCKS_BROKEN.invoker().explode(instance, getBlocksToDestroy());
+    @Inject(method = "shouldDamage", at = @At("HEAD"), cancellable = true)
+    private void shouldDamageMixin(Explosion explosion, Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        final ActionResult result = ExplosionEvents.BEFORE_DAMAGE_ENTITY.invoker().damage(explosion, entity);
+        if (result == ActionResult.FAIL) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
     }
 
 }
