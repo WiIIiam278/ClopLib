aa98d31bb44de1c659a38803d08ab1d447e24894
William278 <will27528@gmail.com>
2025-04-20 15:01:20 +0100

    feat: bukkit event handling for boat, minecart damage events

--- bukkit/src/main/java/net/william278/cloplib/listener/BukkitEntityDamageListener.java
+++ bukkit/src/main/java/net/william278/cloplib/listener/BukkitEntityDamageListener.java
@@ -25,15 +25,19 @@ import net.william278.cloplib.operation.OperationType;
 import net.william278.cloplib.operation.OperationUser;
 import org.bukkit.entity.*;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.projectiles.BlockProjectileSource;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
@@ -44,19 +48,19 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         // Protect against player & projectile-player damage source
         final Optional<Player> damaging = getPlayerSource(e.getDamager());
         if (damaging.isPresent()) {
-            this.handlePlayerDamager(damaging.get(), e);
+            this.handlePlayerDamager(damaging.get(), e.getEntity(), e);
             return;
         }
         if (e.getDamager() instanceof Projectile projectile) {
-            this.handleProjectileDamager(projectile, e);
+            this.handleProjectileDamager(projectile, e.getEntity(), e);
             return;
         }
 
         // Protect against mobs being hurt by explosions
         final EntityDamageEvent.DamageCause cause = e.getCause();
         if (cause == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION
-            || cause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION
-               && !(e.getEntity() instanceof Monster)) {
+                || cause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION
+                && !(e.getEntity() instanceof Monster)) {
             if (getHandler().cancelOperation(Operation.of(
                     OperationType.EXPLOSION_DAMAGE_ENTITY,
                     getPosition(e.getEntity().getLocation())
@@ -71,11 +75,11 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         // Protect against players & projectiles lighting players on fire
         final Optional<Player> damager = getPlayerSource(e.getCombuster());
         if (damager.isPresent()) {
-            this.handlePlayerDamager(damager.get(), e);
+            this.handlePlayerDamager(damager.get(), e.getEntity(), e);
             return;
         }
         if (e.getCombuster() instanceof Projectile projectile) {
-            this.handleProjectileDamager(projectile, e);
+            this.handleProjectileDamager(projectile, e.getEntity(), e);
         }
     }
 
@@ -109,8 +113,32 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         }
     }
 
-    private <E extends EntityEvent & Cancellable> void handlePlayerDamager(@NotNull Player damager, @NotNull E e) {
-        final Optional<Player> damaged = getPlayerSource(e.getEntity());
+    // Handle breaking boats & mine carts
+    @EventHandler(ignoreCancelled = true)
+    default void onVehicleDamage(@NotNull VehicleDamageEvent e) {
+        this.handleVehicleDamage(e.getVehicle(), e.getAttacker(), e);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    default void onVehicleDestroy(@NotNull VehicleDestroyEvent e) {
+        this.handleVehicleDamage(e.getVehicle(), e.getAttacker(), e);
+    }
+
+    // Treat boat collision breaking entities (e.g. running over an item frame) like a monster damaging terrain
+    @EventHandler(ignoreCancelled = true)
+    default void onVehicleCollision(@NotNull VehicleEntityCollisionEvent e) {
+        if (getHandler().cancelOperation(Operation.of(
+                getPlayerSource(e.getVehicle()).map(this::getUser).orElse(null),
+                OperationType.MONSTER_DAMAGE_TERRAIN,
+                getPosition(e.getVehicle().getLocation())
+        ))) {
+            e.setCancelled(true);
+        }
+    }
+
+    private <E extends Event & Cancellable> void handlePlayerDamager(@NotNull Player damager, @NotNull Entity entity,
+                                                                     @NotNull E e) {
+        final Optional<Player> damaged = getPlayerSource(entity);
         if (damaged.isPresent()) {
             if (getHandler().cancelOperation(Operation.of(
                     getUser(damager),
@@ -126,21 +154,22 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         // Determine the Operation type based on the entity being damaged
         if (getHandler().cancelOperation(Operation.of(
                 getUser(damager),
-                getPlayerDamageType(e.getEntity()),
-                getPosition(e.getEntity().getLocation())
+                getPlayerDamageType(entity),
+                getPosition(entity.getLocation())
         ))) {
             e.setCancelled(true);
         }
     }
 
-    private <E extends EntityEvent & Cancellable> void handleProjectileDamager(@NotNull Projectile proj, @NotNull E e) {
+    private <E extends Event & Cancellable> void handleProjectileDamager(@NotNull Projectile proj, @NotNull Entity entity,
+                                                                         @NotNull E e) {
         // Prevent projectiles dispensed outside of claims from harming stuff in claims
         if (proj.getShooter() instanceof BlockProjectileSource shooter) {
             final OperationPosition blockLocation = getPosition(shooter.getBlock().getLocation());
             if (getHandler().cancelNature(
                     blockLocation.getWorld(),
                     blockLocation,
-                    getPosition(e.getEntity().getLocation())
+                    getPosition(entity.getLocation())
             )) {
                 e.setCancelled(true);
             }
@@ -148,14 +177,36 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         }
 
         // Prevent projectiles shot by mobs from harming passive mobs, hanging entities & armor stands
-        if (!(e.getEntity() instanceof Player || e.getEntity() instanceof Monster)
-            && proj.getShooter() instanceof Monster) {
+        if (!(entity instanceof Player || entity instanceof Monster)
+                && proj.getShooter() instanceof Monster) {
             if (getHandler().cancelOperation(Operation.of(
                     OperationType.MONSTER_DAMAGE_TERRAIN,
-                    getPosition(e.getEntity().getLocation())
+                    getPosition(entity.getLocation())
             ))) {
                 e.setCancelled(true);
             }
+        }
+    }
+
+    default <E extends Event & Cancellable> void handleVehicleDamage(@NotNull Vehicle vehicle, @Nullable Entity attacker,
+                                                                     @NotNull E event) {
+        final Optional<Player> damaging = getPlayerSource(attacker);
+        if (damaging.isPresent()) {
+            this.handlePlayerDamager(damaging.get(), vehicle, event);
+            return;
+        }
+        if (attacker instanceof Projectile projectile) {
+            this.handleProjectileDamager(projectile, vehicle, event);
+            return;
+        }
+
+        // Protect against mobs being hurt by explosions
+        if (getHandler().cancelOperation(Operation.of(
+                OperationType.BREAK_VEHICLE,
+                getPosition(vehicle.getLocation()),
+                true
+        ))) {
+            event.setCancelled(true);
         }
     }
 
@@ -167,7 +218,7 @@ public interface BukkitEntityDamageListener extends BukkitListener {
         } else if (entity instanceof Vehicle vehicle && !(entity instanceof Mob)) {
             type = vehicle instanceof InventoryHolder ? OperationType.BLOCK_BREAK : OperationType.BREAK_VEHICLE;
         } else if (entity instanceof LivingEntity living && !living.getRemoveWhenFarAway()
-                   || entity.getCustomName() != null) {
+                || entity.getCustomName() != null) {
             type = OperationType.PLAYER_DAMAGE_PERSISTENT_ENTITY;
         }
         return type;
